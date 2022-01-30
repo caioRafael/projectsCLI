7b4f5547e3b6a87fe9d8904a2619c492b5311af6
caioRafael <caiorafaelrg@gmail.com>
2022-01-30 01:30:33 -0300

    first command

--- src/projects_cli.py
+++ src/projects_cli.py
@@ -1,8 +1,30 @@
 import os
+import argparse
+
+user = os.getlogin()
+path = f'/home/{user}/Área\ de\ Trabalho'
 
 class ProjectsCLI:
-    path = '/home/caio/Área\ de\ Trabalho'
+    CLI_VERSION = "1.0.0"
+    INI_FILE = "projects.ini"
 
-    os.system(f'ls {path}')
 
-    print("olá CLI")
+    def __init__(self):
+        self.__run()
+
+
+    def __run(self):
+        self.parser = argparse.ArgumentParser(
+            prog="project",
+            description="Automação para a criação de projetos pessoais(React, node, python e outras linguagens)",
+            epilog="Desenvolvido por: Caio Rafael",
+            usage="%(prog)s [options]")
+
+        self.parser.version = self.CLI_VERSION
+        self.parser.add_argument("-v", "--version", action="version")
+
+        parser_args = self.parser.parse_args()
+
+        # if parser_args:
+        #     if parser_args.teste:
+        #         print("teste de execução")
