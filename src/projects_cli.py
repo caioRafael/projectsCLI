0c89150a9e1fbd22f6c573d90bcb0ce0ae6f7457
caioRafael <caiorafaelrg@gmail.com>
2022-01-31 00:26:30 -0300

    inicializando projeto node

--- src/projects_cli.py
+++ src/projects_cli.py
@@ -23,8 +23,23 @@ class ProjectsCLI:
         self.parser.version = self.CLI_VERSION
         self.parser.add_argument("-v", "--version", action="version")
 
+        subparser = self.parser.add_subparsers(help="Ações Projeto")
+
+        self.__create_project_parser(subparser)
+
         parser_args = self.parser.parse_args()
 
-        # if parser_args:
-        #     if parser_args.teste:
-        #         print("teste de execução")
+        if parser_args: 
+            if parser_args.node:
+                self.__create_node_project()
+                print("ola")
+                
+    def __create_project_parser(self, subparser):
+        self.project_parser = subparser.add_parser("projeto", help="Gerar projeto")
+        self.project_parser.add_argument("--node", "-n", help="gerador de projeto Node.JS", action="store_true", required=False)
+
+    def __create_node_project(self):
+        os.system(f"mkdir {path}/teste")
+        os.system(f"cd {path}/teste && yarn init -y && ls")
+
+
